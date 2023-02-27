5b09c57ce0ce880f8f3f1b0486d8da36860c92d3
Ivan <ivangiannini1993@gmail.com>
2023-02-27 13:58:26 -0300

    task2 : se agregan archivos js

---
+++ upcoming.js
@@ -0,0 +1,37 @@
+const eventsData = data.events;
+let cardsContainer = document.getElementById('card-container');
+let fragment = document.createDocumentFragment();
+let actualDate = new Date(data.currentDate);
+
+for (let i = 0; i < eventsData.length; i++) {
+    let eventDate = new Date(eventsData[i].date);
+    if (eventDate > actualDate) {
+        let cardDiv = document.createElement('div');
+        cardDiv.classList.add('card', 'p-2');
+        cardDiv.style.width = '18rem';
+        let cardBody = document.createElement('div');
+        cardBody.classList.add('card-body');
+        let cardInfo = document.createElement('div');
+        cardInfo.classList.add('d-flex', 'justify-content-between');
+
+        let cardImg = data.events[i].image;
+        cardDiv.innerHTML = `<img src=${cardImg} class="card-img-top img-main" alt="...">`;
+        let cardTitle = data.events[i].name;
+        let cardDescription = data.events[i].description;
+        cardBody.innerHTML = `<h5 class="card-title">${cardTitle}</h5>`;
+        cardBody.innerHTML += `<p>${cardDescription}</p>`;
+        let price = data.events[i].price;
+        cardInfo.innerHTML = `<p class="card-text">Price: $${price}</p>`;
+        cardInfo.innerHTML += `<a href="./details.html" class="btn btn-primary card-btn-main">Go somewhere</a>`;
+
+        cardDiv.appendChild(cardBody);
+        cardBody.appendChild(cardInfo);
+        fragment.appendChild(cardDiv);
+    }
+}
+
+cardsContainer.appendChild(fragment);
+
+
+
+
